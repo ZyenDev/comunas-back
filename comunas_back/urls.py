d5ab370af217c600cf0dcd9826feb6f88a621ae7
Richard <rsfontecha@gmail.com>
2025-03-04 18:15:40 -0400

    - Se instalo drf_yasg - Se reparo la ruta de documentacion - Se elimina el rastro de CoreAPI

--- comunas_back/urls.py
+++ comunas_back/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework.documentation import include_docs_urls    OBSOLETO!!!
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Comunas",
+      default_version='v1',
+      description="Documentación de la API de Comunas",
+      terms_of_service="https://www.google.com/policies/terms/",
+    #   contact=openapi.Contact(email="contact@comunas.local"),
+    #   license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+#    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +38,6 @@ urlpatterns = [
     path('api/habitantes/', include('apps.DatosHabitante.urls')),
     path('api/ubicaciones/', include('apps.DatosUbicacion.urls')),
     path('api/viviendas/', include('apps.DatosVivienda.urls')),
-    # path('docs/', include_docs_urls(title='API Comunas')),    OBSOLETO!!!
+
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
