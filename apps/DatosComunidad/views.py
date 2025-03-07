1d532f8a13329fa3e416c87c37c74abd0c77aa70
Richard <rsfontecha@gmail.com>
2025-03-07 03:12:54 -0400

    - Cambios en viewset de DatosComunidad

--- apps/DatosComunidad/views.py
+++ apps/DatosComunidad/views.py
@@ -4,10 +4,12 @@ from .serializers import ComunaSerializer, ConsejoComunalSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
 
+
 @permission_classes([IsAuthenticated])
 class ComunaViewSet(viewsets.ModelViewSet):
     queryset = Comuna.objects.all()
     serializer_class = ComunaSerializer
+
 
 @permission_classes([IsAuthenticated])
 class ConsejoComunalViewSet(viewsets.ModelViewSet):
