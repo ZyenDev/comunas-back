8b89c8d25782137a491ca25f71bbcf4ab1574140
Richard <rsfontecha@gmail.com>
2025-05-10 22:09:08 -0400

    - Se agrega funcionalidad en REST_FRAMEWORK

--- comunas_back/settings.py
+++ comunas_back/settings.py
@@ -84,6 +84,7 @@ CORS_ALLOW_HEADERS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
