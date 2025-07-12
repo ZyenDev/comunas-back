64df0da83abd7a3d33b57aec2cd6151f6214776b
JunoWasTalking1012 <JunoWasTalking@proton.me>
2025-07-12 14:59:22 -0400

    feat: añadido Consejo Comunal data a ReporteAdministrador respuestas

--- apps/DatosReportes/views.py
+++ apps/DatosReportes/views.py
@@ -17,6 +17,7 @@ class ReporteAdministrador(ViewSet):
         genero = request.data.get("genero")  # 'M' o 'F' o None
         comuna_id = request.data.get("comuna_id")  # ID de la comuna
 
+        ConsejoComunal = apps.get_model('DatosComunidad', 'ConsejoComunal')
         Habitante = apps.get_model('DatosHabitante', 'Habitante')
         Discapacidad = apps.get_model('DatosHabitante', 'HabitanteDiscapacidad')
         Vivienda = apps.get_model('DatosVivienda', 'Vivienda')
@@ -24,6 +25,9 @@ class ReporteAdministrador(ViewSet):
         # Reporte: Todos los habitantes que pertenecen a una etnia
         if tipo_reporte == "habitantes_etnia":
             qs = Habitante.objects.filter(pertenece_etnia=True)
+            qd = Vivienda.objects.filter(id_vivienda=qs.first().id_vivienda.id_vivienda)
+            qx = ConsejoComunal.objects.filter(id_consejo_comunal=qd.first().id_consejo_comunal_id)
+            consejo = qx.first().nombre
             if comuna_id:
                 qs = qs.filter(id_vivienda__id_consejo_comunal__id_comuna=comuna_id)
             if genero:
@@ -37,11 +41,16 @@ class ReporteAdministrador(ViewSet):
                 "pertenece_etnia",
                 "habitanteetnia__id_etnia__nombre"
             ))
+            for item in data:
+                item["Consejo_Comunal"] = consejo
             return Response({"tipo_reporte": "habitantes_etnia", "data": data}, status=200)
         
         # Reporte: Todos los habitantes con discapacidad
         elif tipo_reporte == "habitantes_discapacidad":
             qs = Habitante.objects.filter(discapacidad=True)
+            qd = Vivienda.objects.filter(id_vivienda=qs.first().id_vivienda.id_vivienda)
+            qx = ConsejoComunal.objects.filter(id_consejo_comunal=qd.first().id_consejo_comunal_id)
+            consejo = qx.first().nombre
             if comuna_id:
                 qs = qs.filter(id_vivienda__id_consejo_comunal__id_comuna=comuna_id)
             if genero:
@@ -53,6 +62,8 @@ class ReporteAdministrador(ViewSet):
                 "primer_apellido", 
                 "sexo"
                 ))
+            for item in data:
+                item["Consejo_Comunal"] = consejo
             return Response({"tipo_reporte": "habitantes_discapacidad", "data": data}, status=200)
     
         # Reporte: Todos los habitantes con discapacidad específica
@@ -62,6 +73,10 @@ class ReporteAdministrador(ViewSet):
                 discapacidad=True,
                 habitantediscapacidad__id_tipo_discapacidad__nombre=discapacidad_nombre
             )
+            qd = Vivienda.objects.filter(id_vivienda=qs.first().id_vivienda.id_vivienda)
+            qx = ConsejoComunal.objects.filter(id_consejo_comunal=qd.first().id_consejo_comunal_id)
+            consejo = qx.first().nombre
+            
             if comuna_id:
                 qs = qs.filter(id_vivienda__id_consejo_comunal__id_comuna=comuna_id)
             if genero:
@@ -72,8 +87,10 @@ class ReporteAdministrador(ViewSet):
                 "primer_nombre",
                 "primer_apellido",
                 "sexo",
-                "habitantediscapacidad__id_tipo_discapacidad__nombre"
             ))
+            # Add 'Consejo_Comunal' to each item in the data
+            for item in data:
+                item["Consejo_Comunal"] = consejo
             return Response({"tipo_reporte": "habitantes_discapacidad_especifica", "data": data}, status=200)
 
         # Reporte: Todos los habitantes de la tercera edad
@@ -81,6 +98,10 @@ class ReporteAdministrador(ViewSet):
             hoy = date.today()
             fecha_limite = hoy.replace(year=hoy.year - 60)
             qs = Habitante.objects.filter(fecha_nacimiento__lte=fecha_limite)
+            qd = Vivienda.objects.filter(id_vivienda=qs.first().id_vivienda.id_vivienda)
+            qx = ConsejoComunal.objects.filter(id_consejo_comunal=qd.first().id_consejo_comunal_id)
+            consejo = qx.first().nombre
+            
             if comuna_id:
                 qs = qs.filter(id_vivienda__id_consejo_comunal__id_comuna=comuna_id)
             if genero:
@@ -93,6 +114,8 @@ class ReporteAdministrador(ViewSet):
             "edad",
             "sexo"
             ))
+            for item in data:
+                item["Consejo_Comunal"] = consejo
             return Response({"tipo_reporte": "habitantes_tercera_edad", "data": data}, status=200)
 
         # Reporte: Todos los habitantes menores de edad
@@ -103,6 +126,10 @@ class ReporteAdministrador(ViewSet):
             except ValueError:
                 fecha_limite = hoy.replace(month=2, day=28, year=hoy.year - 18)
             qs = Habitante.objects.filter(fecha_nacimiento__gt=fecha_limite)
+            qd = Vivienda.objects.filter(id_vivienda=qs.first().id_vivienda.id_vivienda)
+            qx = ConsejoComunal.objects.filter(id_consejo_comunal=qd.first().id_consejo_comunal_id)
+            consejo = qx.first().nombre
+            
             if comuna_id:
                 qs = qs.filter(id_vivienda__id_consejo_comunal__id_comuna=comuna_id)
             if genero:
@@ -115,11 +142,17 @@ class ReporteAdministrador(ViewSet):
                 "edad",
                 "sexo"
             ))
+            for item in data:
+                item["Consejo_Comunal"] = consejo
             return Response({"tipo_reporte": "habitantes_menores_edad", "data": data}, status=200)
 
         # Reporte: Todos los habitantes y su nivel de estudio
         elif tipo_reporte == "habitantes_nivel_estudio":
             qs = Habitante.objects.exclude(habitantenivelestudio=None)
+            qd = Vivienda.objects.filter(id_vivienda=qs.first().id_vivienda.id_vivienda)
+            qx = ConsejoComunal.objects.filter(id_consejo_comunal=qd.first().id_consejo_comunal_id)
+            consejo = qx.first().nombre
+   
             if comuna_id:
                 qs = qs.filter(id_vivienda__id_consejo_comunal__id_comuna=comuna_id)
             if genero:
@@ -132,6 +165,8 @@ class ReporteAdministrador(ViewSet):
                 "sexo",
                 "habitantenivelestudio__id_nivel_estudio__nombre"
             ))
+            for item in data:
+                item["Consejo_Comunal"] = consejo
             return Response({"tipo_reporte": "habitantes_nivel_estudio", "data": data}, status=200)
 
         # Reporte: Todos los habitantes y su nivel de estudio específico
@@ -140,6 +175,10 @@ class ReporteAdministrador(ViewSet):
             qs = Habitante.objects.filter(
                 habitantenivelestudio__id_nivel_estudio__nombre=nivel_nombre
             )
+            qd = Vivienda.objects.filter(id_vivienda=qs.first().id_vivienda.id_vivienda)
+            qx = ConsejoComunal.objects.filter(id_consejo_comunal=qd.first().id_consejo_comunal_id)
+            consejo = qx.first().nombre
+   
             if comuna_id:
                 qs = qs.filter(id_vivienda__id_consejo_comunal__id_comuna=comuna_id)
             if genero:
@@ -152,6 +191,8 @@ class ReporteAdministrador(ViewSet):
                 "sexo",
                 "habitantenivelestudio__id_nivel_estudio__nombre"
             ))
+            for item in data:
+                item["Consejo_Comunal"] = consejo
             return Response({"tipo_reporte": "habitantes_nivel_estudio_especifico", "data": data}, status=200)
 
         # Reporte: Todas las viviendas con "x cantidad de habitantes"
